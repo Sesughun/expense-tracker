448fb57c8c921d0514a4d1606308c0a4ddbc9d90
Sesughun <sesughunlan@gmail.com>
2024-07-08 11:18:33 -0700

    working tracker, first can submit and then calculate the total

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import "./App.css";
-import React, { useState, useRef, useEffect, useContext } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import AddCategory from "./addCategory";
 import ExpenseContext from "./ExpenseContext";
 
 function App() {
+  const now = new Date();
   const [category, setCategory] = useState(["Groceries", "Vehicle"]);
+  const descriptionEl = useRef();
+  const amountEl = useRef();
+  const categoryEl = useRef();
+  const categoryEl2 = useRef();
+  const [total, setTotal] = useState(300);
   const table_data = [
     {
+      date: {
+        currentDate: now.toLocaleDateString(),
+        currentTime: now.toLocaleTimeString(),
+      },
       description: "Item 1",
       amount: 100,
       category: category[0],
     },
     {
+      date: {
+        currentDate: now.toLocaleDateString(),
+        currentTime: now.toLocaleTimeString(),
+      },
       description: "Item 2",
       amount: 200,
       category: category[1],
     },
   ];
-  const categoryEl = useRef();
+
   const [table, setTable] = useState(table_data);
 
   function handleAddCategory() {
@@ -31,12 +45,27 @@ function App() {
   useEffect(() => {}, [category]);
 
   function handleSubmit() {
-    //setTable()
+    const amounts = parseFloat(amountEl.current.value);
+    const newTableSet = {
+      date: {
+        currentDate: now.toLocaleDateString(),
+        currentTime: now.toLocaleTimeString(),
+      },
+      description: descriptionEl.current.value,
+      amount: amountEl.current.value,
+      category: categoryEl2.current.value,
+    };
+    setTable((prevTable) => [...prevTable, newTableSet]);
+    descriptionEl.current.value = "";
+    amountEl.current.value = "";
+    categoryEl2.current.value = "";
+    setTotal((prevAmount) => prevAmount + amounts);
   }
   function handleDelete() {}
 
   return (
     <div className="container">
+      <h1>Milan's Expense Tracker</h1>
       <ExpenseContext.Provider
         value={{ category, setCategory, categoryEl, handleAddCategory }}
       >
@@ -50,6 +79,7 @@ function App() {
             id="description"
             placeholder="Enter item description"
             name="Description"
+            ref={descriptionEl}
           />
         </div>
         <div className="mb-3">
@@ -64,13 +94,14 @@ function App() {
             name="Amount"
             min={100}
             step={100}
+            ref={amountEl}
           />
         </div>
         <div className="mb-3">
           <label for="category" className="form-label">
             Category:
           </label>
-          <select class="form-select">
+          <select class="form-select" ref={categoryEl2}>
             <option>Add category</option>
             {category.map((cates) => (
               <option>{cates}</option>
@@ -83,9 +114,10 @@ function App() {
         <AddCategory />
 
         <div>
-          View Category
-          <select class="form-select">
-            <option>Add category</option>
+          <h2 style={{ margin: "1rem" }}>View Category</h2>
+
+          <select class="form-select" placeholder="Add Category">
+            <option>All</option>
             {category.map((cates) => (
               <option>{cates}</option>
             ))}
@@ -93,8 +125,9 @@ function App() {
           <table className="table table-bordered w-100" id="table-text">
             <thead>
               <tr>
+                <th>Date/Time</th>
                 <th>Description</th>
-                <th>Amount</th>
+                <th>Amount(NGN)</th>
                 <th>Category</th>
                 <th></th>
               </tr>
@@ -102,6 +135,11 @@ function App() {
             <tbody>
               {table.map((item, index) => (
                 <tr key={index}>
+                  <td>
+                    {item.date.currentDate}
+                    <br />
+                    {item.date.currentTime}
+                  </td>
                   <td>{item.description}</td>
                   <td>{item.amount}</td>
                   <td>{item.category}</td>
@@ -112,6 +150,17 @@ function App() {
                   </td>
                 </tr>
               ))}
+              <tr>
+                <td></td>
+                <td>
+                  <b>Total:</b>
+                </td>
+                <td>
+                  <b>{total}</b>
+                </td>
+                <td></td>
+                <td></td>
+              </tr>
             </tbody>
           </table>
         </div>
